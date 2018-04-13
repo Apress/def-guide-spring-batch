9062c33964a99630ba53d0926c4fb408936763dc
Michael Minella <mminella@pivotal.io>
2018-04-12 21:48:49 -0500

    Initial commit

---
+++ Chapter9/src/main/java/com/apress/batch/chapter9/Chapter9Application.java
@@ -0,0 +1,12 @@
+package com.apress.batch.chapter9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter9Application.class, args);
+	}
+}
