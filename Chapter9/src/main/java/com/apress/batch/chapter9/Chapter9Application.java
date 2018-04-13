ad1faa844e73786cf0c2f9f0286d98a606c862d8
Michael Minella <mminella@pivotal.io>
2018-04-12 22:24:45 -0500

    Formatted Text File Job

--- Chapter9/src/main/java/com/apress/batch/chapter9/Chapter9Application.java
+++ Chapter9/src/main/java/com/apress/batch/chapter9/Chapter9Application.java
@@ -1,12 +1,21 @@
 package com.apress.batch.chapter9;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class Chapter9Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Chapter9Application.class, args);
+		List<String> newArgs = new ArrayList<>(3);
+		newArgs.add("customerFile=/data/customer.csv");
+		newArgs.add("outputFile=file:/Users/mminella/Documents/IntelliJWorkspace/def-guide-spring-batch/Chapter9/target/formattedCustomers.txt");
+
+		SpringApplication.run(Chapter9Application.class, newArgs.toArray(new String[newArgs.size()]));
 	}
 }
---
+++ Chapter9/src/main/java/com/apress/batch/chapter9/domain/Customer.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.apress.batch.chapter9.domain;
+
+import java.io.Serializable;
+
+/**
+ * @author Michael Minella
+ */
+public class Customer implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private long id;
+	private String firstName;
+	private String middleInitial;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleInitial() {
+		return middleInitial;
+	}
+
+	public void setMiddleInitial(String middleInitial) {
+		this.middleInitial = middleInitial;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+}
