16af325c1d57c455a9eb57b03944793498d72087
Michael Minella <mminella@pivotal.io>
2018-05-08 22:09:23 -0500

    Added email job

--- Chapter9/src/main/java/com/apress/batch/chapter9/Chapter9Application.java
+++ Chapter9/src/main/java/com/apress/batch/chapter9/Chapter9Application.java
@@ -13,7 +13,7 @@ public class Chapter9Application {
 
 	public static void main(String[] args) {
 		List<String> newArgs = new ArrayList<>(3);
-		newArgs.add("customerFile=/data/customer.csv");
+		newArgs.add("customerFile=/data/customerWithEmail.csv");
 		newArgs.add("outputFile=file:/Users/mminella/Documents/IntelliJWorkspace/def-guide-spring-batch/Chapter9/target/formattedCustomers.xml");
 
 		SpringApplication.run(Chapter9Application.class, newArgs.toArray(new String[newArgs.size()]));
